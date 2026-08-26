e3e47504e7e31bfc426f211900df35fe37719453
MilitantNoob <rohitkumartharpyal7070@gmail.com>
2026-08-27 00:13:11 +0530

    Update Gzip-compressed WebGL build with Devnet Solana NFT verification and strict access gating

--- Build/Builds.loader.js
+++ Build/Builds.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 123284425,
+    wasmFileSize: 137608742,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
